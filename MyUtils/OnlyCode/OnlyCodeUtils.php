5df1bdc7eb4d6d7a23a631c9d0c412cbca732ce8
cathy <1574829575@qq.com>
2021-09-13 10:26:04 +0800

    修改配置文件

--- MyUtils/OnlyCode/OnlyCodeUtils.php
+++ MyUtils/OnlyCode/OnlyCodeUtils.php
@@ -1,12 +1,12 @@
 <?php
-namespace MyUtils\Library;
+namespace MyUtils;
 
 class OnlyCodeUtils
 {
     /**
      * 生成20位唯一订单号
      */
-    public static function generateOrderNumber(): string
+    public static function generateOrderNumber()
     {
         return date('YmdHis').rand(100000,999999);
     }
