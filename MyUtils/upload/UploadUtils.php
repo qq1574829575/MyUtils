d666f626c59159aaeb315688aee8fb15a236c142
cathy <1574829575@qq.com>
2021-11-16 09:43:47 +0800

    修复多文件上传

--- MyUtils/upload/UploadUtils.php
+++ MyUtils/upload/UploadUtils.php
@@ -17,37 +17,32 @@ class UploadUtils
         $limit_file_ext = array("jpg","jpeg","png","gif"),
         $use_https_url = false
     ){
-        $files = $_FILES;
+        $files = $_FILES["files"];
         $arr_urls = array();
 
         if (!is_dir($save_upload_path)){
             return array("code"=>0,"message"=>"无效的上传文件夹！");
         }
 
-        if (count($files)<=0){
+        if ($files == null || count($files["name"]) == 0){
             return array("code"=>0,"message"=>"未上传文件！");
         }
 
-        foreach ($files as $file){
-            $file_name = $file[ 'name' ]; // 文件名称
-            $file_size = $file[ 'size' ]; // 文件大小
+        for ($i = 0;$i < count($files["name"]); $i++){
+            $file_name = $files[ 'name' ][$i]; // 文件名称
+            $file_size = $files[ 'size' ][$i]; // 文件大小
             $file_ext  = strtolower(substr($file_name, strrpos($file_name, '.') + 1)); // 文件后缀名
-            $file_tmp  = $file['tmp_name']; // 临时文件地址
-
+            $file_tmp  = $files['tmp_name'][$i]; // 临时文件地址
             if ($file_size > 1024 * 1024 * $limit_file_size){
                 return array("code"=>0,"message"=>"上传文件大小超过限制");
             }
-
             if (!self::checkFileExt($file_ext, $limit_file_ext)){
                 return array("code"=>0,"message"=>"不允许上传.".$file_ext."类型的文件");
             }
-
             self::checkPictureFile($file_ext,$file_tmp);
-
             if(!rename($file_tmp,$save_upload_path ."/". $file_name)) {
                 return array("code"=>0,"message"=>"保存文件失败");
             }
-
             if ($is_return_url){
                 $httpHeader = "http";
                 if ($use_https_url){
